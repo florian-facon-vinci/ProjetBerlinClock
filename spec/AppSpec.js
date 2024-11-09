4c19ac98d3195e9ea4f5b3c49dfce4901defaac8
aurelien-fontana-vinci <aurelien.fontana@student.vinci.be>
2024-11-09 15:47:12 +0100

    add :test 1 (minute 1)

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -2,7 +2,13 @@ import {Main} from '../src/berlinClock.js';
 
 describe("berlinClock function should return time", function (){
 
-    it("nom du test ",function (){
 
+    it("test minute 1 ",function (){
+        let main = new Main();
+
+        const date = new Date();
+        const result = main.berlinClock(date.getMinutes());
+        
+         expect(result).toBe(true);
     });
-})
+});
--- src/berlinClock.js
+++ src/berlinClock.js
@@ -1,5 +1,11 @@
-export class Main{
-    berlinClock(){
-        
-    }
+export class Main{  
+
+
+    berlinClock(date){
+        var l1_1 = false;
+
+        if(date === 1 ) l1_1 = !l1_1;
+
+        return l1_1;
+    } 
 }
